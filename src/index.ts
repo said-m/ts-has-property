b44e374b4b51ee81e70cee8fefaffdc86fa0b638
Said Magomedov <linpark1999@gmail.com>
2019-09-06 20:59:18 +0300

    ✨ Проверка наличия значения - булевый 3-ий аргумент функции

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,73 @@
 import { isArray, isPlainObject } from '@said-m/common';
 import { ObjectInterface } from '@said-m/common/dist/interfaces';
 
-/**
- * Indicating whether the object has the
- * specified property as its own property
- */
-export default function hasProperty<
-  // tslint:disable-next-line: no-any
-  T extends ObjectInterface<any>,
+export default hasProperty;
+// tslint:disable-next-line: no-any
+type FirstArgumentInterface = ObjectInterface<any>;
+
+/** Проверка наличия ключа */
+function hasProperty<
+  T extends FirstArgumentInterface,
   K extends keyof T
 >(
   object: T,
   property: K | Array<K>,
+  isExist: void,
 ): object is (
   {
     [key in keyof T]: T[key];
   } & {
     [key in K]: T[key];
   }
+);
+
+/** Проверка наличия ключа и его значения */
+function hasProperty<
+  T extends FirstArgumentInterface,
+  K extends keyof T
+>(
+  object: T,
+  property: K | Array<K>,
+  isExist: boolean,
+): object is (
+  {
+    [key in keyof T]: T[key];
+  } &
+  {
+    [key in K]: Exclude<T[key], undefined | null>;
+  }
+);
+
+/**
+ * Indicating whether the object has the
+ * specified property as its own property
+ */
+function hasProperty<
+  T extends FirstArgumentInterface,
+  K extends keyof T
+>(
+  object: T,
+  property: K | Array<K>,
+  isRequired: boolean | void,
 ) {
   const properties = isArray(property)
     ? property
     : [property];
 
   return properties.every(
-    thisProperty => isPlainObject(object) &&
-      Object.hasOwnProperty.call(object, thisProperty),
+    thisProperty => {
+      if (!isPlainObject(object)) {
+        return false;
+      }
+
+      if (isRequired === true) {
+        const value = object[thisProperty];
+
+        return value !== undefined &&
+        value !== null;
+      }
+
+      return Object.hasOwnProperty.call(object, thisProperty);
+    },
   );
 }
